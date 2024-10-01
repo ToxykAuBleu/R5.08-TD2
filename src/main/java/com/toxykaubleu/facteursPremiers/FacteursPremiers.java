c17857b2cd1b352b4067bb3eee38df2eff97f12a
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-10-01 09:55:24 +0200

    🔥 Initialisation du dépôt.

---
+++ src/main/java/com/toxykaubleu/facteursPremiers/FacteursPremiers.java
@@ -0,0 +1,4 @@
+package com.toxykaubleu.facteursPremiers;
+
+public class FacteursPremiers {
+}
---
+++ src/test/java/com/toxykaubleu/facteursPremiers/FacteursPremiersTest.java
@@ -0,0 +1,8 @@
+package com.toxykaubleu.facteursPremiers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class FacteursPremiersTest {
+
+}
