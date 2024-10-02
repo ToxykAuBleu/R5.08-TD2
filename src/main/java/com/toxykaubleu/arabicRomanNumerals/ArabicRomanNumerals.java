b6768377c4918d442a78d1c67e387b51c06eb056
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-10-02 14:56:47 +0200

    👍 Implémentation de la méthode convert().

--- src/main/java/com/toxykaubleu/arabicRomanNumerals/ArabicRomanNumerals.java
+++ src/main/java/com/toxykaubleu/arabicRomanNumerals/ArabicRomanNumerals.java
@@ -1,7 +1,40 @@
 package com.toxykaubleu.arabicRomanNumerals;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.UnknownFormatConversionException;
+import java.util.stream.Stream;
+
 public class ArabicRomanNumerals {
+    private static HashMap<Integer, String> mapped = generateMap();
+
+    private static HashMap<Integer, String> generateMap() {
+        HashMap<Integer, String> result = new LinkedHashMap<>();
+        result.put(1000, "M");
+        result.put(500, "D");
+        result.put(100, "C");
+        result.put(90, "XC");
+        result.put(50, "L");
+        result.put(40, "XL");
+        result.put(10, "X");
+        result.put(9, "IX");
+        result.put(5, "V");
+        result.put(4, "IV");
+        result.put(1, "I");
+        return result;
+    }
     public static String convert(Integer n) {
-        return null;
+        String result = "";
+        for (Map.Entry<Integer, String> entry : mapped.entrySet()) {
+            Integer key = entry.getKey();
+            String val = entry.getValue();
+            while (n >= key) {
+                result += val;
+                n -= key;
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/com/toxykaubleu/arabicRomanNumerals/ArabicRomanNumeralsTest.java
+++ src/test/java/com/toxykaubleu/arabicRomanNumerals/ArabicRomanNumeralsTest.java
@@ -1,17 +1,24 @@
 package com.toxykaubleu.arabicRomanNumerals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 class ArabicRomanNumeralsTest {
-    @Test()
-    void convert_devrait_retourner_traduction_nombre_en_chiffre_romain() {
-        // GIVEN
-        Integer param = 3;
+    @ParameterizedTest()
+    @CsvSource({
+            "1, 'I'",
+            "3, 'III'",
+            "4, 'IV'",
+            "10, 'X'",
+            "39, 'XXXIX'",
+            "40, 'XL'",
+    })
+    void convert_devrait_retourner_traduction_nombre_en_chiffre_romain(Integer param, String excepted) {
         // WHEN
         var result = ArabicRomanNumerals.convert(param);
         // THEN
-        assertThat(result).isEqualTo("III");
+        assertThat(result).isEqualTo(excepted);
     }
 }
